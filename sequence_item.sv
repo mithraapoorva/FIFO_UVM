34120be350edd3ab89dbf8461feecc592f0e17fb
mithraapoorva <116264695+mithraapoorva@users.noreply.github.com>
2023-09-11 15:55:15 +0530

    Update sequence_item.sv

--- sequence_item.sv
+++ sequence_item.sv
@@ -8,47 +8,47 @@ class fifo_seq_item extends uvm_sequence_item;
   //---------------------------------------
   //data and control fields
   //---------------------------------------
-  rand bit [7:0]data_in;
-  rand bit rd;
-  rand bit wr;
-  bit full;
-  bit empty;
-  bit [7:0]data_out;
+  rand bit [127:0] i_wrdata;;
+  rand bit  i_rden;
+  rand bit  i_wren;
+  bit  o_full;
+  bit  o_empty;
+  bit  o_alm_full;
+  bit  o_alm_empty;
+  bit   [127:0]o_rddata;
   
-  //---------------------------------------
+
   //Utility and Field macros
-  //---------------------------------------
+
   `uvm_object_utils_begin(fifo_seq_item)
-  `uvm_field_int(data_in, UVM_ALL_ON)
-  `uvm_field_int(rd, UVM_ALL_ON)
-  `uvm_field_int(wr, UVM_ALL_ON)
-  `uvm_field_int(full, UVM_ALL_ON)
-  `uvm_field_int(empty, UVM_ALL_ON)
-  `uvm_field_int(data_out, UVM_ALL_ON)
+  `uvm_field_int( i_wrdata, UVM_ALL_ON)
+  `uvm_field_int( i_rden, UVM_ALL_ON)
+  `uvm_field_int(i_wren, UVM_ALL_ON)
+  `uvm_field_int(o_full, UVM_ALL_ON)
+  `uvm_field_int(o_empty, UVM_ALL_ON)
+   `uvm_field_int(o_alm_full, UVM_ALL_ON)
+  `uvm_field_int(o_alm_empty, UVM_ALL_ON)
+  `uvm_field_int(o_rddata, UVM_ALL_ON)
   `uvm_object_utils_end
-  
-  //---------------------------------------
-  //Constraint
-  //---------------------------------------
-  constraint c1{rd!=wr;};
-  
-  //---------------------------------------
+
+
   //Pre randomize function
-  //---------------------------------------
+
   function void pre_randomize();
-    if(rd)
+    if( i_rden)
       data_in.rand_mode(0);
   endfunction
   
-  //---------------------------------------
+
   //Constructor
-  //---------------------------------------
+
   function new(string name="fifo_seq_item");
     super.new(name);
   endfunction
   
   function string convert2string();
-    return $psprintf("data_in=%0h,data_out=%0h,wr=%0d,rd=%0d,full=%od,empty=%0d",data_in,data_out,wr,rd,full,empty);
+    return
+    $psprintf("i_wrdata=%0h,o_rddata=%0h,i_wren=%0d,i_rden=%0d,o_full=%od,o_empty=%0d,o_alm_full=%0d,o_alm_empty=%0d",i_wrdata,o_rddata,i_wren,i_rden,o_full,o_empty,o_alm_full,o_alm_empty);
   endfunction
   
 endclass
