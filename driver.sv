0be6a03b8f297b0b7940dc2a1f16e82263053292
mithraapoorva <116264695+mithraapoorva@users.noreply.github.com>
2023-09-12 12:11:21 +0530

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -14,14 +14,14 @@ class f_driver extends uvm_driver#(f_sequence_item);
   endfunction
 
   virtual task run_phase(uvm_phase phase);
-    vif.d_mp.d_cb.wr <= 'b0;
-    vif.d_mp.d_cb.rd <= 'b0;
+    vif.d_mp.d_cb.i_wren <= 'b0;
+    vif.d_mp.d_cb.i_rden <= 'b0;
     vif.d_mp.d_cb.data_in <= 'b0;
     forever begin
       seq_item_port.get_next_item(req);
-      if(req.wr == 1)
+      if(req.i_wren == 1)
         main_write(req.data_in);
-      else if(req.rd == 1)
+      else if(req.i_rden == 1)
         main_read();
       seq_item_port.item_done();
     end
@@ -29,17 +29,17 @@ class f_driver extends uvm_driver#(f_sequence_item);
   
   virtual task main_write(input [7:0] din);
     @(posedge vif.d_mp.clk)
-    vif.d_mp.d_cb.wr <= 'b1;
+    vif.d_mp.d_cb.i_wren <= 'b1;
     vif.d_mp.d_cb.data_in <= din;
     @(posedge vif.d_mp.clk)
-    vif.d_mp.d_cb.wr <= 'b0;
+    vif.d_mp.d_cb.i_wren <= 'b0;
   endtask
   
   virtual task main_read();
     @(posedge vif.d_mp.clk)
-    vif.d_mp.d_cb.rd <= 'b1;
+    vif.d_mp.d_cb.i_rden <= 'b1;
     @(posedge vif.d_mp.clk)
-    vif.d_mp.d_cb.rd <= 'b0;
+    vif.d_mp.d_cb.i_rden <= 'b0;
   endtask
 
 endclass
