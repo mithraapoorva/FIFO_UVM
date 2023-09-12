69eeac40441ffe386ab4b7addbed032b2ed8497a
mithraapoorva <116264695+mithraapoorva@users.noreply.github.com>
2023-09-12 11:16:00 +0530

    Update sequence.sv

--- sequence.sv
+++ sequence.sv
@@ -27,11 +27,22 @@ class f_sequence extends uvm_sequence#(f_sequence_item);
       assert(req.randomize());
       finish_item(req);
     end
-         `uvm_info(get_type_name(), $sformatf("******** Generate simaltaneous 20   write and read ********"), UVM_LOW)
+      `uvm_info(get_type_name(), $sformatf("******** Generate simaltaneous 20   write and read ********"), UVM_LOW)
       repeat(20) begin
       req = f_sequence_item::type_id::create("req");
       start_item(req);
         assert(req.randomize()with {i_rden=1;i_wren=1;});
+      finish_item(req);
+    end
+      `uvm_info(get_type_name(), $sformatf("******** Generate Alternative 20   write and read ********"), UVM_LOW)
+      repeat(20) begin
+      req = f_sequence_item::type_id::create("req");
+      start_item(req);
+        assert(req.randomize()with {i_rden=0;i_wren=1;});
+      finish_item(req);
+         req = f_sequence_item::type_id::create("req");
+      start_item(req);
+        assert(req.randomize()with {i_rden=1;i_wren=0;});
       finish_item(req);
     end
      `uvm_info(get_type_name(), $sformatf("******** Generate 20 no read and write at same time ******"), UVM_LOW)
@@ -42,7 +53,6 @@ class f_sequence extends uvm_sequence#(f_sequence_item);
       finish_item(req);
     end                                 
   endtask
-  
-endclass
+  endclass
   
   
