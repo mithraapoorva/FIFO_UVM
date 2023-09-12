58e484129271cad3859f6b859cada39eafc0dae2
mithraapoorva <116264695+mithraapoorva@users.noreply.github.com>
2023-09-12 15:09:40 +0530

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -1,33 +1,22 @@
-interface f_interface(input clk, reset);
+interface f_interface(input clk, rstn);
   bit i_wren;
   bit i_rden;
-  bit [127:0] data_in;
-  bit  o_full;
+  bit [127:0] i_wrdata;
+  bit o_full;
   bit o_empty;
   bit o_alm_full;
   bit o_alm_empty;
-bit [127:0]o_rddata;
+  bit [127:0] o_rddata;
   
-  clocking driver_cb @(posedge clk);
-    default input #1 output #1;
+  clocking d_cb @(posedge clk);
+    default input #1 output #0;
     output i_wren;
     output i_rden;
-    output data_in;
-    input o_full;
-    input o_empty;
-    input  o_alm_full;
-    input o_alm_empty;
-    input o_rddata;
+    output i_wrdata;
   endclocking
   
-  clocking active_monitor_cb @(posedge clk);
-    default input #0 output #0;
-    input i_wren;
-    input i_rden;
-    input i_wrdata;
-  endclocking
-  
-clocking passive_monitor_cb @(posedge clk);
+  clocking m_cb @(posedge clk);
+    default input #1 output #0;
     input o_full;
     input o_empty;
     input o_alm_full;
@@ -35,8 +24,7 @@ clocking passive_monitor_cb @(posedge clk);
     input o_rddata;
   endclocking
   
-modport driver_mp (input clk, reset, clocking driver_cb);
-  modport act_monitor_mp (input clk, reset, clocking active_monitor_cb);
-  modport act_monitor_mp (input clk, reset, clocking passive_monitor_cb);
+  modport d_mp (input clk, rstn, clocking d_cb);
+  modport m_mp (input clk, rstn, clocking m_cb);
     
 endinterface
