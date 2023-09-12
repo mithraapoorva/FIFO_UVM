80d246fa22bb941f0dc4a5515555d7b460cec342
mithraapoorva <116264695+mithraapoorva@users.noreply.github.com>
2023-09-12 15:11:54 +0530

    Update Sequencer.sv

--- Sequencer.sv
+++ Sequencer.sv
@@ -1,8 +1,8 @@
-class f_sequencer extends uvm_sequencer#(f_sequence_item);
-  `uvm_component_utils(f_sequencer)
-  
-  function new(string name = "f_sequencer", uvm_component parent);
+class fifo_sequencer extends uvm_sequencer#(fifo_seq_item);
+  `uvm_component_utils(fifo_sequencer)
+
+  function new(string name = "fifo_sequencer", uvm_component parent);
     super.new(name, parent);
   endfunction
-  
+
 endclass
