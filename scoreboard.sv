04e211cde0e6ff6ead3229ec864b7c83c1830a18
mithraapoorva <116264695+mithraapoorva@users.noreply.github.com>
2023-09-13 10:43:44 +0530

    Update scoreboard.sv

--- scoreboard.sv
+++ scoreboard.sv
@@ -1,9 +1,9 @@
 class f_scoreboard extends uvm_scoreboard;
-  uvm_analysis_imp#(f_sequence_item, f_scoreboard) item_got_export;
+  uvm_analysis_imp#(fifo_seq_item , f_scoreboard) item_got_export;
   `uvm_component_utils(f_scoreboard)
   
   function new(string name = "f_scoreboard", uvm_component parent);
-    super.new(name, parent);
+    super.new(name , parent);
     item_got_export = new("item_got_export", this);
   endfunction
   
@@ -13,17 +13,17 @@ class f_scoreboard extends uvm_scoreboard;
   
   int queue[$];
   
-  function void write(input f_sequence_item item_got);
+  function void write(input fifo_seq_item item_got);
     bit [7:0] examdata;
-    if(item_got.wr == 'b1)begin
-      queue.push_back(item_got.data_in);
-      `uvm_info("write Data", $sformatf("wr: %0b rd: %0b data_in: %0h full: %0b",item_got.wr, item_got.rd,item_got.data_in, item_got.full), UVM_LOW);
+    if(item_got.i_wren == 'b1)begin
+      queue.push_back(item_got.i_wrdata);
+      `uvm_info("write Data", $sformatf("i_wren: %0b i_rden: %0b i_wrdata: %0h o_full: %0b  o_alm_full: %0b",item_got.i_wren, item_got.i_rden,item_got.i_wrdata, item_got.o_full, item_got.o_alm_full), UVM_LOW);
     end
-    else if (item_got.rd == 'b1)begin
+    else if (item_got.i_rden == 'b1)begin
       if(queue.size() >= 'd1)begin
         examdata = queue.pop_front();
-        `uvm_info("Read Data", $sformatf("examdata: %0h data_out: %0h empty: %0b", examdata, item_got.data_out, item_got.empty), UVM_LOW);
-        if(examdata == item_got.data_out)begin
+        `uvm_info("Read Data", $sformatf("examdata: %0h o_rddata: %0h o_empty: %0b  o_alm_empty", examdata, item_got.o_rddata, item_got.o_empty , item_got.o_alm_empty), UVM_LOW);
+        if(examdata == item_got.o_rddata)begin
           $display("-------- 		Pass! 		--------");
         end
         else begin
