aa6994922a9c0d938a0f266b1f88d333e60e88db
mithraapoorva <116264695+mithraapoorva@users.noreply.github.com>
2023-09-20 12:07:05 +0530

    Update my_fifo .sv

--- my_fifo .sv
+++ my_fifo .sv
@@ -1,6 +1,6 @@
 module my_fifo #(
                    parameter DATA_W           = 128      ,        // Data width
-                   parameter DEPTH            = 10      ,        // Depth of FIFO                   
+                   parameter DEPTH            = 1024      ,        // Depth of FIFO                   
                    parameter UPP_TH           = 4      ,        // Upper threshold to generate Almost-full
                    parameter LOW_TH           = 2               // Lower threshold to generate Almost-empty
                 )
@@ -28,7 +28,7 @@ logic [DATA_W - 1        : 0] data_rg [DEPTH] ;        // Data array
 logic [$clog2(DEPTH) - 1 : 0] wrptr_rg        ;        // Write pointer
 logic [$clog2(DEPTH) - 1 : 0] rdptr_rg        ;        // Read pointer
 logic [$clog2(DEPTH)     : 0] dcount_rg       ;        // Data counter
-logic ready_rg = 0 ;      
+    
 logic                         wren_s          ;        // Write Enable signal generated iff FIFO is not full
 logic                         rden_s          ;        // Read Enable signal generated iff FIFO is not empty
 logic                         full_s          ;        // Full signal
@@ -105,7 +105,7 @@ assign wren_s      = i_wren & !full_s                               ;
 assign rden_s      = i_rden & !empty_s                              ;
 
 // Full and Empty to output
-assign o_full      = full_s  || !ready_rg                           ;
+assign o_full      = full_s                         ;
 assign o_empty     = empty_s                                        ;
 
 // Almost-full and Almost-empty to output
