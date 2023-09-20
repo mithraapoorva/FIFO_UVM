ed635e346f2b34a6745a10d1fcfe7f0eae5e2dbc
mithraapoorva <116264695+mithraapoorva@users.noreply.github.com>
2023-09-20 16:47:21 +0530

    Update subscribe.sv

--- subscribe.sv
+++ subscribe.sv
@@ -1,12 +1,12 @@
-class fifo_subscriber extends uvm_subscriber#(transaction);
+class fifo_subscriber extends uvm_subscriber#(fifo_seq_item);
 
   `uvm_component_utils (fifo_subscriber)
 
- 
 
-  uvm_analysis_imp#(transaction, fifo_subscriber) item_got_export1;
 
-  transaction tr;
+  uvm_analysis_imp#(fifo_seq_item, fifo_subscriber) item_got_export1;
+
+  fifo_seq_item tr;
 
 
 covergroup cg;
@@ -16,6 +16,13 @@ covergroup cg;
   write:coverpoint tr.i_wren;
 
   data: coverpoint tr.i_wrdata;
+
+endgroup
+
+
+
+
+covergroup eg;
 
   full: coverpoint tr.o_full;
 
@@ -28,34 +35,47 @@ covergroup cg;
   dataout : coverpoint tr.o_rddata;
 
 endgroup
-
- 
-
-function new(string name="f_subscriber",uvm_component parent);
+  function new(string name="f_subscriber",uvm_component parent);
 
   super.new(name,parent);
 
   item_got_export1= new("item_got_export1", this);
 
-  tr=transaction::type_id::create("tr");
+  tr=fifo_seq_item::type_id::create("tr");
 
   cg=new();
-
+  eg= new();
 endfunction
 
- 
 
-  function void write(transaction t);
+
+
+  function void write(fifo_seq_item t);
 
    tr=t;
 
    cg.sample();
 
-   $display("coverage =%0d ",cg.get_coverage());
+   $display("inputcoverage =%0d ",cg.get_coverage());
+
+   eg.sample();
+   $display("output coverage =%0d ",eg.get_coverage());
+
+endfunction
+
+n new(string name="f_subscriber",uvm_component parent);
+
+  super.new(name,parent);
+
+  item_got_export2= new("item_got_export2", this);
+
+  tr=fifo_seq_item::type_id::create("tr");
+ cg=new();
 
 endfunction
 
 
- 
+
 
 endclass
+          
